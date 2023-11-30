dc01b4825228cf180278d71e8b36c3bc205a9fdc
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-30 23:27:52 +0900

    에러
    npm run dev 시 Error: error:0308010C:digital envelope routines::unsupported

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // import Vue from "vue";
 import { createApp } from "vue";
 import App from "./App";
+import router from "./routes/index.js";
 
-createApp(App).mount("#app");
+
+createApp(App).use(router).mount("#app");
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view />
+  <h1>App.vue</h1>
 </template>
 
 <script>
